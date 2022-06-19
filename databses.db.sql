a635ff904c6df3e1e2391f2d862245bc836620b9
poulpe <poulpe@localhost.localdomain>
2022-06-19 16:26:22 +0200

    [Update] Fix database script

--- databses.db.sql
+++ databses.db.sql
@@ -1,24 +1,26 @@
-BEGIN TRANSACTION;
+CREATE DATABASE IF NOT EXISTS vpn;
+USE vpn;
 CREATE TABLE IF NOT EXISTS cert (
-	user_id	TEXT NOT NULL,
-	user_fingerprint	TEXT NOT NULL,
+	user_id	VARCHAR(128) NOT NULL UNIQUE,
+	user_fingerprint	VARCHAR(128) NOT NULL,
 	user_validate	INT DEFAULT 1,
 	PRIMARY KEY(user_id)
 );
 CREATE TABLE IF NOT EXISTS groups (
 	group_id	INT NOT NULL,
-	name	TEXT NOT NULL,
+	name	VARCHAR(128) NOT NULL,
 	PRIMARY KEY(group_id)
 );
 CREATE TABLE IF NOT EXISTS groups_member (
 	user_id	INT NOT NULL,
 	group_id	INT NOT NULL,
+	CONSTRAINT groups_member_user
 	FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 CREATE TABLE IF NOT EXISTS users (
 	id_row	INT NOT NULL,
-	user_id	TEXT NOT NULL UNIQUE,
-	user_pass	TEXT NOT NULL,
-	user_pass_salt	TEXT NOT NULL,
+	user_id	VARCHAR(128) NOT NULL UNIQUE,
+	user_pass	VARCHAR(128) NOT NULL,
+	user_pass_salt	VARCHAR(128) NOT NULL,
 	PRIMARY KEY(id_row AUTOINCREMENT)
 );
