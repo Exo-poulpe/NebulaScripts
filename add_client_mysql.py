fae7f6211af7e4731f1fa0f919e7cd9fead2f52d
poulpe <poulpe@localhost.localdomain>
2022-06-20 08:46:50 +0200

    [Update] Remove simple quote

--- add_client_mysql.py
+++ add_client_mysql.py
@@ -23,7 +23,7 @@ def database_add_user_id(user_id:str,passw:str) -> str():
         salt = bcrypt.gensalt()
         passwd = bcrypt.hashpw(bytes(passw,"utf-8"), salt)
         # print(f"{passwd}   {salt}")
-        cursor.execute(f"INSERT INTO users ('user_id','user_pass','user_pass_salt') VALUES ('{user_id}','{passwd.decode('utf-8')}','{salt.decode('utf-8') }');")
+        cursor.execute(f"INSERT INTO users (user_id,user_pass,user_pass_salt) VALUES ('{user_id}','{passwd.decode('utf-8')}','{salt.decode('utf-8') }');")
         connection.commit()
     except Exception as err:
         return err
@@ -40,7 +40,7 @@ def database_add_groups_for_user(user_id:str,groups:List[str]) -> bool:
         for grp in groups:
             cursor.execute(f"SELECT group_id FROM groups WHERE name = '{grp}';")
             tmp_id = cursor.fetchall()[0][0]
-            cursor.execute(f"INSERT INTO groups_member ('user_id','group_id') VALUES ('{user_id}','{tmp_id}');")
+            cursor.execute(f"INSERT INTO groups_member (user_id,group_id) VALUES ('{user_id}','{tmp_id}');")
             connection.commit()
     except Exception as err:
         return res
